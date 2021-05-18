328a7b8d68f06ad74fa2e1ef18ad3ac7da415161
nos-nart <nosnart1510@gmail.com>
2021-05-18 14:26:46 +0700

    [+] ☘️ api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,15 +3,24 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from './logger/logger.module';
 import { TodosModule } from './modules/todos/todos.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     LoggerModule,
     TodosModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('database.uri'),
+        uri: configService.get<string>('database.uri', ''),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
       }),
       inject: [ConfigService],
     }),
